58d099e5a5d468c1aa1fc1a0bdf6778f30065f6a
YogitaPatil12095 <yogitapatilx12@gmail.com>
2025-07-13 21:47:02 +0530

    Initial commit

---
+++ src/test/java/add_calTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class add_calTest {
+
+    @Test
+    void twoplus4()
+    {
+        var obj = new add_cal();
+        assertEquals(4,obj.add(2,2));
+    }
+
+    @Test
+    void fourfourpluseight()
+    {
+        var obj = new add_cal();
+       assertEquals(8,obj.add(4,4));
+    }
+
+}
